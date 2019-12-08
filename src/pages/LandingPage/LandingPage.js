bc0fe3b8b07bec85423969fc3b62d3fdb0135ce5
phe0 <pheofo@gmail.com>
2019-12-08 11:58:52 -0300

    project setup and background

---
+++ src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import NameSVG from '../../components/NameSVG/NameSVG'
+import Background from '../../components/Background/Background'
+import './LandingPage.css'
+
+class LandingPage extends Component{
+    render() {
+        return (
+            <>
+                <div className="landing-section">
+                    <Background/>
+                </div>
+                <div className="test-space">
+
+                </div>
+            </>
+        )
+    }
+}
+export default LandingPage
